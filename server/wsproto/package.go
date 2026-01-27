8657608dc79fc3183d9950de01481dc46ad2c1c2
Federico-Giuliani <federico.giuliani@marconirovereto.it>
2026-01-27 10:16:34 +0000

    Feat(wsprotocol): creata la struttura base del packetto per la comunicazione tra server e client

---
+++ server/wsproto/package.go
@@ -0,0 +1,20 @@
+/*
+Wsproto è il packetto responsabile per la struttura del protocollo che Skribio utilizza
+per comunicare tra client e server.
+
+Il protocollo si basa sull'invio di dati tramite JSON formattati in due campi Header e Body.
+
+			+-------------------------------+
+			|			 HEADER 			|
+			|								|
+			|								|
+			|								|
+			|								|
+			+-------------------------------+
+			|			  BODY				|
+			|								|
+			|								|
+			|								|
+			+-------------------------------+
+*/
+package wsproto
---
+++ server/wsproto/wspackets.go
@@ -0,0 +1,3 @@
+package wsproto
+
+type WsPacket struct{}
