a732693eb919e8c644c0a5bf53794d0b0cd8db94
Federico-Giuliani <giuliani.federicomarcogabriele@gmail.com>
2026-01-27 18:28:01 +0100

    Feat(wsproto): implementata la struttura base che verrà mandata tramite ws

--- server/wsproto/wspackets.go
+++ server/wsproto/wspackets.go
@@ -1,3 +1,25 @@
 package wsproto
 
-type WsPacket struct{}
+import (
+	"encoding/json"
+
+	"github.com/team-condominio/skribio/htypes"
+)
+
+// WsPacket è la struttura che viene mandata tramite
+// ws per la comunicazione client-server
+type WsPacket struct {
+	// Contiene il tipo di messaggio che
+	// il client o il server deve interpretare
+	Header htypes.HeaderType
+
+	// Contiene dati aggiuntivi specifici per il tipo
+	// di messaggio ricevuto, codificato in formato JSON.
+	// 
+	// In caso non si voglia mandare informazioni aggiuntive utilizzare:
+	// 	wp := WsPacket{
+	//		Header: TIPO_DA_INSERIRE
+	//		Body: json.RawMessage(`null`)
+	//	} 
+	Body json.RawMessage
+}
